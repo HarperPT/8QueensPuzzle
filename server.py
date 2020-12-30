23ae1dd693bb44dc0ca5c959982f125a7ae0da30
HarperPT <Harperyhpt@gmail.com>
2020-12-30 14:05:54 +0800

    Fix data transfer

--- server.py
+++ server.py
@@ -11,13 +11,14 @@ def index():
   if request.method =='POST':
     queens_count = request.form.get("SltQueens")
     queens_count = int(queens_count)
+    
     if queens_count > 0:
       puzzle = Puzzle(queens_count)
       result = json.loads(puzzle.get_solutions())
-      # print(result)
+
       return render_template("index.html", data = queens_count, result = result)
-  
-  return render_template("index.html", data=null)
+
+  return render_template("index.html")
 
 if __name__ == '__main__':
   app.run(debug=True)
